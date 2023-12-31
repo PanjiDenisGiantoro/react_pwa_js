d7fc4d18a72c3fd6ae4fb70556a9f870f88bc473
PanjiDenisGiantoro <panjidenisgiantoroo@gmail.com>
2023-12-31 16:02:54 +0700

    Asset

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Browser from "./components/Browser";
+import Arrived from "./components/Arrived";
+import Clients from "./components/Clients";
+import AsideMenu from "./components/AsideMenu";
+import Footer from "./components/Footer";
 
 function App() {
     return (
@@ -9,6 +13,10 @@ function App() {
             <Header/>
             <Hero/>
             <Browser/>
+            <Arrived/>
+            <Clients />
+            <AsideMenu />
+            <Footer />
         </>
     );
 }
